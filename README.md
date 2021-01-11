babd69ea8287d8c012caa5b889ff5bebce68cead
DemonFox <1196752479@qq.com>
2021-01-11 20:19:38 +0800

    更改数据库为sqlite，不用配置mysql能运行

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
-# 1.Windows下配置MySQL
+# 觉得有用请点赞
 
-## 1.1 下载MySQL
+## 1. 使用sqlite作为数据库，则不需要配置mySQL
+```
+参考settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'student_crud'),
+    }
+}
+```
+## 2. 开始使用
+> pycharm cmd
+```
+python manage makemigrations
+python manage migrate
+```
+> windows cmd(命令行)
+```
+python .\manage.py makemigrations
+python .\manage.py migrate
+```
 
-## 1.2 初始化服务器
-
-## 1.3 更改用户密码
-
-## 1.4 创建表格
-
-# 2. PyMySQL
-
-## 2.1如果出现某个版本不一致问题
-
-> pip install pymysql
-
-## 2.2 在mysql中创建数据库
-
-## 2.3 迁移数据库
--- StudentCRUD/settings.py
+++ StudentCRUD/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'cqpmen&l^p@c+227)c%*xx9phpd6858t^(3dau^@+i7hmg9-fh'
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
 
 # Application definition
 
@@ -71,24 +69,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'StudentCRUD.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-import pymysql      # 注意要加上这些
-pymysql.install_as_MySQLdb()     # 注意要加上这些
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'student_crud',    # 数据库名
-        'USER': 'root',     # 数据库用户名，得先存在
-        'PASSWORD': 'password',  # 数据库密码
-        'HOST': '127.0.0.1',   # 数据库地址
-        'PORT': '3306', # 数据库端口号，默认3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'student_crud'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -108,11 +98,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans' # en-us 英文| zh-Hans 中文
+LANGUAGE_CODE = 'zh-Hans'  # en-us 英文| zh-Hans 中文
 
 TIME_ZONE = 'UTC'
 
@@ -121,7 +110,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
@@ -133,4 +121,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 # 图片上传的路径
-MEDIA_ROOT=os.path.join(BASE_DIR,"static/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
